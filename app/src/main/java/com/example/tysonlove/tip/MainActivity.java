5a22a3b13ff078f1bb151507984e473663879ebe
Lester Chavez <lchavez9669@conestogac.on.ca>
2017-09-15 19:44:35 -0400

    Before implementing the onClick method

--- app/src/main/java/com/example/tysonlove/tip/MainActivity.java
+++ app/src/main/java/com/example/tysonlove/tip/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.tysonlove.tip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity  {
+
+    Button btnFivePercent;
+    Button btnTenPercent;
+    Button btnFifteenPercent;
+    Button btnTwentyPercent;
+    TextView txtViewBottom;
+    EditText editText;
+    Double money;
+    Double finalAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFivePercent = (Button) findViewById(R.id.btnFivePercent);
+        btnTenPercent = (Button) findViewById(R.id.btnTenPercent);
+        btnFifteenPercent = (Button) findViewById(R.id.btnFifteenPercent);
+        btnTwentyPercent = (Button) findViewById(R.id.btnTwentyPercent);
+        txtViewBottom = (TextView) findViewById(R.id.txtViewBottom);
+        editText = (EditText) findViewById(R.id.editText);
+
+        finalAmount = 1.00;
     }
 }
